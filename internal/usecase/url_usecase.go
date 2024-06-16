fc7ff5c80d0f1b751995867f2c5c2a67cc8143c7
Ержан <a1@MacBook-Pro-1.local>
2024-06-16 15:24:24 +0500

    8 commit iter4

--- internal/usecase/url_usecase.go
+++ internal/usecase/url_usecase.go
@@ -8,21 +8,32 @@ import (
 	"github.com/YerzhanAkhmetov/go-shortener/internal/repository"
 )
 
+// URLUsecase представляет интерфейс для работы с сокращением URL.
 type URLUsecase interface {
+	// Create создает новую запись сокращенного URL на основе оригинального URL.
+	// Возвращает созданный объект domain.URL и ошибку, если возникла.
 	Create(originalURL string) (domain.URL, error)
+
+	// GetByID возвращает сокращенный URL по его идентификатору.
+	// Возвращает найденный объект domain.URL и флаг, указывающий на наличие записи.
 	GetByID(id string) (domain.URL, bool)
 }
 
+// urlUsecase представляет реализацию интерфейса URLUsecase.
 type urlUsecase struct {
-	repo repository.URLRepository
+	repo repository.URLRepository // Репозиторий для работы с хранилищем URL
 }
 
+// NewURLUsecase создает новый экземпляр URLUsecase.
 func NewURLUsecase(repo repository.URLRepository) URLUsecase {
 	return &urlUsecase{repo: repo}
 }
 
+// Create создает новую запись сокращенного URL на основе оригинального URL.
+// Генерирует уникальный идентификатор, сохраняет URL в репозитории.
+// Возвращает созданный объект domain.URL и ошибку, если возникла.
 func (u *urlUsecase) Create(originalURL string) (domain.URL, error) {
-	id, err := generateID()
+	id, err := generateID() // Генерация уникального идентификатора
 	if err != nil {
 		return domain.URL{}, err
 	}
@@ -30,22 +41,28 @@ func (u *urlUsecase) Create(originalURL string) (domain.URL, error) {
 		ID:          id,
 		OriginalURL: originalURL,
 	}
-	err = u.repo.Save(url)
+	err = u.repo.Save(url) // Сохранение URL в репозитории
 	if err != nil {
 		return domain.URL{}, err
 	}
 	return url, nil
 }
 
+// GetByID возвращает сокращенный URL по его идентификатору.
+// Использует репозиторий для поиска URL по заданному идентификатору.
+// Возвращает найденный объект domain.URL и флаг, указывающий на наличие записи.
 func (u *urlUsecase) GetByID(id string) (domain.URL, bool) {
 	return u.repo.FindByID(id)
 }
 
+// generateID генерирует случайный уникальный идентификатор длиной 6 байт.
+// Использует криптографический рандом для генерации.
+// Возвращает сгенерированный идентификатор в виде строки и ошибку, если возникла.
 func generateID() (string, error) {
 	b := make([]byte, 6)
-	_, err := rand.Read(b)
+	_, err := rand.Read(b) // Генерация случайных байт с использованием криптографического рандома
 	if err != nil {
 		return "", err
 	}
-	return base64.URLEncoding.EncodeToString(b), nil
+	return base64.URLEncoding.EncodeToString(b), nil // Кодирование в base64 для представления в виде строки
 }
--- tests/handler_test.go
+++ tests/handler_test.go
@@ -19,7 +19,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// TestCreateShortURLHandler проверяет обработчик CreateShortURL
 func TestCreateShortURLHandler(t *testing.T) {
+	// Создание временного хранилища, репозитория, usecase и обработчика
 	store := storage.NewMemoryStorage()
 	repo := repository.NewURLRepository(store)
 	urlUsecase := usecase.NewURLUsecase(repo)
@@ -28,56 +30,67 @@ func TestCreateShortURLHandler(t *testing.T) {
 	}
 	h := handler.NewHandler(urlUsecase, cfg)
 
+	// Создание маршрутизатора и установка обработчика для POST запросов
 	r := mux.NewRouter()
 	r.HandleFunc("/", h.CreateShortURL).Methods("POST")
 
+	// Определение ожидаемых результатов для различных тестовых случаев
 	type want struct {
-		contentType string
-		statusCode  int
-		body        interface{}
+		contentType string      // Ожидаемый тип содержимого ответа
+		statusCode  int         // Ожидаемый HTTP статус код
+		body        interface{} // Ожидаемое тело ответа
 	}
 	tests := []struct {
-		name string
-		body string
-		want want
+		name string // Название тестового случая
+		body string // Тело HTTP запроса
+		want want   // Ожидаемые результаты
 	}{
 		{
-			name: "valid URL",
-			body: "https://practicum.yandex.ru/",
+			name: "valid URL",                    // Название теста: валидный URL
+			body: "https://practicum.yandex.ru/", // Тело запроса: валидный URL
 			want: want{
-				contentType: "text/plain",
-				statusCode:  http.StatusCreated,
-				body:        nil,
+				contentType: "text/plain",       // Ожидаемый тип содержимого: текстовый plain
+				statusCode:  http.StatusCreated, // Ожидаемый HTTP статус код: 201 Created
+				body:        nil,                // Ожидаемое тело: пустое (т.к. тут ожидается короткий URL)
 			},
 		},
 		{
-			name: "empty body",
-			body: "",
+			name: "empty body", // Название теста: пустое тело запроса
+			body: "",           // Тело запроса: пустое
 			want: want{
-				contentType: "application/json",
-				statusCode:  http.StatusBadRequest,
-				body:        errs.NewError("Invalid request body", http.StatusBadRequest, "Bad Request"),
+				contentType: "application/json",                                                          // Ожидаемый тип содержимого: JSON
+				statusCode:  http.StatusBadRequest,                                                       // Ожидаемый HTTP статус код: 400 Bad Request
+				body:        errs.NewError("Invalid request body", http.StatusBadRequest, "Bad Request"), // Ожидаемое тело: ошибка "Invalid request body"
 			},
 		},
 	}
+
+	// Итерация по всем тестовым случаям
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Создание HTTP POST запроса с указанным телом
 			request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(tt.body))
 			w := httptest.NewRecorder()
 			r.ServeHTTP(w, request)
 
+			// Получение результата HTTP запроса
 			result := w.Result()
 			defer result.Body.Close()
 
+			// Проверка ожидаемого HTTP статус кода
 			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			// Проверка ожидаемого типа содержимого ответа
 			assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
 
+			// Если ожидается статус код 201 (Created), проверяем содержимое тела ответа
 			if tt.want.statusCode == http.StatusCreated {
 				body, err := io.ReadAll(result.Body)
 				require.NoError(t, err)
 				shortURL := string(body)
+				// Проверка, что возвращенный короткий URL начинается с заданного базового URL
 				assert.True(t, strings.HasPrefix(shortURL, "http://localhost:8080/"))
 			} else if tt.want.body != nil {
+				// Если ожидается тело с ошибкой, проверяем JSON формат тела ответа
 				body, err := io.ReadAll(result.Body)
 				require.NoError(t, err)
 				expectedBody, err := json.Marshal(tt.want.body)
@@ -88,7 +101,9 @@ func TestCreateShortURLHandler(t *testing.T) {
 	}
 }
 
+// TestRedirectHandler проверяет обработчик Redirect
 func TestRedirectHandler(t *testing.T) {
+	// Создание временного хранилища, репозитория, usecase и обработчика
 	store := storage.NewMemoryStorage()
 	repo := repository.NewURLRepository(store)
 	urlUsecase := usecase.NewURLUsecase(repo)
@@ -97,52 +112,62 @@ func TestRedirectHandler(t *testing.T) {
 	}
 	h := handler.NewHandler(urlUsecase, cfg)
 
+	// Сохранение тестовой ссылки в хранилище
 	store.SaveURL("test1", "https://practicum.yandex.ru/")
 
+	// Создание маршрутизатора и установка обработчика для GET запросов
 	r := mux.NewRouter()
 	r.HandleFunc("/{id}", h.Redirect).Methods("GET")
 
+	// Определение ожидаемых результатов для различных тестовых случаев
 	type want struct {
-		statusCode int
-		location   string
-		body       interface{}
+		statusCode int         // Ожидаемый HTTP статус код
+		location   string      // Ожидаемое значение заголовка Location для перенаправления
+		body       interface{} // Ожидаемое тело ответа
 	}
 	tests := []struct {
-		name    string
-		request string
-		want    want
+		name    string // Название тестового случая
+		request string // Путь для HTTP GET запроса
+		want    want   // Ожидаемые результаты
 	}{
 		{
-			name:    "URL found",
-			request: "/test1",
+			name:    "URL found", // Название теста: найден URL
+			request: "/test1",    // Путь запроса: /test1
 			want: want{
-				statusCode: http.StatusTemporaryRedirect,
-				location:   "https://practicum.yandex.ru/",
-				body:       nil,
+				statusCode: http.StatusTemporaryRedirect,   // Ожидаемый HTTP статус код: 307 Temporary Redirect
+				location:   "https://practicum.yandex.ru/", // Ожидаемое значение заголовка Location
+				body:       nil,                            // Ожидаемое тело ответа: пустое (т.к. ожидается перенаправление)
 			},
 		},
 		{
-			name:    "URL not found",
-			request: "/nonexistent",
+			name:    "URL not found", // Название теста: URL не найден
+			request: "/nonexistent",  // Путь запроса: /nonexistent
 			want: want{
-				statusCode: http.StatusNotFound,
-				body:       errs.NewError("URL not found", http.StatusNotFound, "Not Found"),
+				statusCode: http.StatusNotFound,                                              // Ожидаемый HTTP статус код: 404 Not Found
+				body:       errs.NewError("URL not found", http.StatusNotFound, "Not Found"), // Ожидаемое тело ответа: ошибка "URL not found"
 			},
 		},
 	}
+
+	// Итерация по всем тестовым случаям
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Создание HTTP GET запроса с указанным путем
 			request := httptest.NewRequest(http.MethodGet, tt.request, nil)
 			w := httptest.NewRecorder()
 			r.ServeHTTP(w, request)
 
+			// Получение результата HTTP запроса
 			result := w.Result()
 			defer result.Body.Close()
 
+			// Проверка ожидаемого HTTP статус кода
 			assert.Equal(t, tt.want.statusCode, result.StatusCode)
 			if tt.want.location != "" {
+				// Если ожидается перенаправление, проверяем заголовок Location
 				assert.Equal(t, tt.want.location, result.Header.Get("Location"))
 			} else if tt.want.body != nil {
+				// Если ожидается тело с ошибкой, проверяем JSON формат тела ответа
 				body, err := io.ReadAll(result.Body)
 				require.NoError(t, err)
 				expectedBody, err := json.Marshal(tt.want.body)
